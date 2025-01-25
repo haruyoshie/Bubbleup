8f1d0f81616ace792eca84f515447c73eb4cb87d
Dafovi <38021029+Dafovi@users.noreply.github.com>
2025-01-25 10:13:11 -0500

    GameManager add

---
+++ Bubble up/Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public event Action<float> Height;
+}
